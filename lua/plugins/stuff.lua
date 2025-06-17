1f320bc47785b2a6134745fdcadb3c9f37fb701f
andrew0451 <kalebrenp@gmail.com>
2025-06-17 21:02:01 +0100

    Initial commit

---
+++ lua/plugins/stuff.lua
@@ -0,0 +1,33 @@
+return {
+	-- colorscheme
+	{
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		-- This command sets the colorscheme when the plugin loads
+		vim.cmd("colorscheme rose-pine")
+	end,
+	},
+	-- nvim web icons
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+
+	-- Render Markdown
+	{
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+},
+
+	-- neorg
+	{
+	"nvim-neorg/neorg",
+	lazy = false, -- leave disabled
+	version = "*",
+	config = true,
+},
+
+}
