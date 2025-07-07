e0b608ab21e292103b3331b4f65ca6d23ef31909
andrew0451 <kalebrenp@gmail.com>
2025-07-07 21:30:54 +0100

    feat: Modified dashboard

--- lua/plugins/alpha-dashboard.lua
+++ lua/plugins/alpha-dashboard.lua
@@ -1,6 +1,46 @@
 return {
     'goolord/alpha-nvim',
     config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        local dashboard = require'alpha.themes.dashboard'
+        local startify = require'alpha.themes.startify'
+
+        -- ASCII Art Header
+        dashboard.section.header.val = [[
+            █▒▒▒     █▒▒ █▒▒▒▒▒▒▒▒     █▒▒▒▒      █▒▒         █▒▒ █▒▒ █▒▒       █▒▒
+ █▒ █▒▒   █▒▒ █▒▒         █▒▒    █▒▒    █▒▒       █▒▒  █▒▒ █▒ █▒▒   █▒▒▒
+ █▒▒ █▒▒  █▒▒ █▒▒       █▒▒        █▒▒   █▒▒     █▒▒   █▒▒ █▒▒ █▒▒ █ █▒▒
+ █▒▒  █▒▒ █▒▒ █▒▒▒▒▒▒   █▒▒        █▒▒    █▒▒   █▒▒    █▒▒ █▒▒  █▒▒  █▒▒
+ █▒▒   █▒ █▒▒ █▒▒       █▒▒        █▒▒     █▒▒ █▒▒     █▒▒ █▒▒   █▒  █▒▒
+ █▒▒    █▒ ▒▒ █▒▒         █▒▒     █▒▒       █▒▒▒▒      █▒▒ █▒▒       █▒▒
+ █▒▒      █▒▒ █▒▒▒▒▒▒▒▒     █▒▒▒▒            █▒▒       █▒▒ █▒▒       █▒▒
+
+        ]]
+
+        -- Buttons
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+            dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("u", "  Update plugins", ":Lazy sync<CR>"),
+            dashboard.button("q", "  Quit", ":qa<CR>"),
+        }
+
+        -- Recently Used Files
+        dashboard.section.mru = startify.section.mru
+        dashboard.section.mru.opts = {
+            max_items = 5
+        }
+
+        -- Set layout and setup
+        dashboard.opts.layout = {
+            { type = "padding", val = 2 },
+            dashboard.section.header,
+            { type = "padding", val = 2 },
+            dashboard.section.buttons,
+            { type = "padding", val = 1 },
+            dashboard.section.mru,
+        }
+
+        require'alpha'.setup(dashboard.opts)
     end
-};
+}
