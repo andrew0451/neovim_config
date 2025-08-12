114f883258dcd6d4e0945f541cce53469b02db2b
andrew0451 <kalebrenp@gmail.com>
2025-08-12 11:55:43 -0500

    feat: Remove toggleterm, replace with floaterm.lua

---
+++ lua/plugins/floaterm.lua
@@ -0,0 +1,13 @@
+return {
+  "voldikss/vim-floaterm",
+  config = function()
+    vim.g.floaterm_width = 0.9
+    vim.g.floaterm_height = 0.8
+    vim.g.floaterm_wintype = "float"
+    vim.g.floaterm_position = "center"
+
+    vim.keymap.set('n', '<leader>t', ':FloatermToggle<CR>', { noremap = true, silent = true, desc = "Toggle Floaterm" })
+    vim.keymap.set('n', '<leader>ft', ':FloatermNew<CR>', { noremap = true, silent = true, desc = "New Floaterm" })
+    vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>:FloatermToggle<CR>]], { noremap = true, silent = true })
+  end,
+}
