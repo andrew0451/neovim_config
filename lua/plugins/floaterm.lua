bb9d9db8f9d9d4e954873885d80cf72dcfa2a7f4
andrew0451 <kalebrenp@gmail.com>
2025-11-05 21:48:06 -0600

    remove lsp.lua

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -7,7 +7,7 @@ return {
     vim.g.floaterm_position = "center"
 
     vim.keymap.set('n', '<C-T>', ':FloatermToggle<CR>', { noremap = true, silent = true, desc = "Toggle Floaterm" })
-    vim.keymap.set('t', '<C-F>', ':FloatermNext<CR>', { noremap = true, silent = true, desc = "Toggle Floaterm" })
+  --  vim.keymap.set('t', '<C-F>', ':FloatermNext<CR>', { noremap = true, silent = true, desc = "Toggle Floaterm" })
     vim.keymap.set('n', '<C-N>', ':FloatermNew<CR>', { noremap = true, silent = true, desc = "New Floaterm" })
     vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>:FloatermToggle<CR>]], { noremap = true, silent = true })
   end,
